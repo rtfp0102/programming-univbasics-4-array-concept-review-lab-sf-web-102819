9d717007c7517105af832a4cf67611c1627aadc0
rtfp0102 <rtfp0102@gmail.com>
2019-10-01 02:55:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,13 +18,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = nil
+  min = 0
   counter = 0 
   
   while counter < array.length do
     element = array[counter]
     
-    if (element < min || min == nil)
+    if element < min 
       min = element 
     end
     
